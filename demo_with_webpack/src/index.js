477d00fce008352bc6563d36ccf8057569adf7ba
Simon Neutert <simonneutert@users.noreply.github.com>
2021-10-26 22:19:18 +0200

    demo update / webpack

--- demo_with_webpack/src/index.js
+++ demo_with_webpack/src/index.js
@@ -1,10 +1,11 @@
-import googlePlaces from 'google-maps-reviews';
-import style from 'google-maps-reviews/google-maps-reviews.css';
+import googlePlaces from "google-maps-reviews";
+// eslint-disable-next-line no-unused-vars
+import style from "google-maps-reviews/google-maps-reviews.css";
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  //Find a placeID via https://developers.google.com/places/place-id
+document.addEventListener(window.google, "DOMContentLoaded", (event) => {
+  console.log(`Plugin loaded: ${event}`);
+  // Find a placeID via https://developers.google.com/places/place-id
   googlePlaces("google-reviews", {
-    placeId: 'ChIJZa6ezJa8j4AR1p1nTSaRtuQ',
+    placeId: "ChIJZa6ezJa8j4AR1p1nTSaRtuQ",
   });
-
 });
--- demo_with_webpack/webpack.config.js
+++ demo_with_webpack/webpack.config.js
@@ -1,18 +1,17 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    }]
-  }
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
