06217af630740adbf47bc20f0d9fad2ceba222db
Simon Neutert <simonneutert@users.noreply.github.com>
2022-02-08 18:56:15 +0100

    updates demo

--- demo_with_webpack/src/index.js
+++ demo_with_webpack/src/index.js
@@ -1,11 +1,14 @@
-import googlePlaces from "google-maps-reviews";
 // eslint-disable-next-line no-unused-vars
 import style from "google-maps-reviews/google-maps-reviews.css";
 
-document.addEventListener(window.google, "DOMContentLoaded", (event) => {
-  console.log(`Plugin loaded: ${event}`);
+import googlePlaces from "../../google-maps-reviews";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log(`Plugin loaded!`);
+  console.log(event);
+  console.log(window.google);
   // Find a placeID via https://developers.google.com/places/place-id
-  googlePlaces("google-reviews", {
-    placeId: "ChIJZa6ezJa8j4AR1p1nTSaRtuQ",
+  googlePlaces(window.google, "google-reviews", {
+    placeId: "ChIJT8RwZwaPsUcRhkKYaCqr5LI",
   });
 });
